c2ae8dabfe5f26213a725d7ca894cc06194f8018
gopilagah15 <150451916+gopilagah@users.noreply.github.com>
2024-03-16 22:12:50 +0530

    djfkjfdkgjd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  const [num, setNum] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <> 
+    
+    <div className="container" style={{width:'100%', height:'100vh'}}>
+    <div style={{position:'absolute', left:'50%' , top:'50%', transform:"translate(-50%,-50%)"}}>
+      <div style={{display:'flex',justifyContent:'center'}}>
+    <Header/>
     </div>
+    <h1 style={{display:'flex',justifyContent:'center'}}>{num}</h1>
+    <button onClick={()=>{setNum(num+1)}}>Increment</button>
+    <button onClick={()=>{setNum(num-1)}}>Decrement</button> 
+    </div>
+    </div>
+    </>
   );
 }
 
 export default App;
+
+
+// let ex = 234;
+// function setEx(n) {ex=n}
+// const useState = [ex,setEx]
+// useState[0]
+// 234
+// setEx(423290249)
+// ex
+// 423290249
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+<div className="">
+    UseState Hook
+</div>
+  )
+}
+
+export default Header
